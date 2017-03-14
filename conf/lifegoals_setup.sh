503a19ff5a46d1ba0777d4c47a04cddf49fd335a
Shipra <code.shipra@gmail.com>
2017-03-14 18:30:59 +0530

    uwsgi related changes

--- conf/lifegoals_setup.sh
+++ conf/lifegoals_setup.sh
@@ -5,11 +5,14 @@ tar -xf redis-3.2.8.tar.gz
 cd redis-3.2.8
 make test, make, make install
 
-yum install python-devel gcc epel-release python-pip nginx -y
+yum install python-devel gcc epel-release python-pip nginx git -y
+pip install virtualenv
 
 cd /opt
 git clone https://github.com/ShipraShalini/lifegoals.git
+virtualenv lifegoals
 cd lifegoals/
+
 pip install -r requirements.txt
 cp conf/nginx.conf /etc/nginx/conf.d/
 pub_ip = "$(curl ipecho.net/plain)"
