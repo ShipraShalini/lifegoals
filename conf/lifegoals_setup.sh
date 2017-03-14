e116a41ab74e72f3afd7d30722e03525bfe093a8
Shipra <code.shipra@gmail.com>
2017-03-14 19:26:31 +0530

    adds echos

--- conf/lifegoals_setup.sh
+++ conf/lifegoals_setup.sh
@@ -1,18 +1,23 @@
 #!/usr/bin/env bash
-# get redis
+
+echo "Installing dependencies"
+yum install python-devel gcc epel-release python-pip nginx git -y
+pip install virtualenv uwsgi
+
+echo "Installing redis..."
 wget http://download.redis.io/releases/redis-3.2.8.tar.gz
 tar -xf redis-3.2.8.tar.gz
 cd redis-3.2.8
 make test, make, make install
 
-yum install python-devel gcc epel-release python-pip nginx git -y
-pip install virtualenv uwsgi
 
+echo "Cloning the repo.."
 cd /opt
 git clone https://github.com/ShipraShalini/lifegoals.git
 virtualenv lifegoals
 cd lifegoals/
 
+echo "Setting up the app..."
 pip install -r requirements.txt
 cp conf/nginx.conf /etc/nginx/conf.d/
 pub_ip = "$(curl ipecho.net/plain)"
